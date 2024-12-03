626d28300a8b55c59fe4a8b689e4bcf61fe99ce1
Lijing <82682@LIJING>
2024-12-03 23:35:49 +0800

    nicec

--- Hello0/Hello0/Program.cs
+++ Hello0/Hello0/Program.cs
@@ -10,7 +10,7 @@ namespace Hello0
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            // Add services to the container.
+            // Add services to the container
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
